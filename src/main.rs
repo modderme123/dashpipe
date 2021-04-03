4a7bea519ee11bdce769c88a4f348d48032df935
modderme123 <modderme123@gmail.com>
2021-04-03 15:19:41 -0700

    daemonization?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,60 @@
 extern crate clap;
 use clap::App;
-use std::io::{self, BufRead};
-use std::net::TcpListener;
-use tungstenite::server::accept;
+use std::net::{TcpListener, TcpStream};
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
+use std::{io::prelude::*, thread::sleep, time::Duration};
 use tungstenite::Message;
+use tungstenite::{server::accept, WebSocket};
+
+use fork::{chdir, close_fd, fork, setsid, Fork};
+
+fn forward(mut a: TcpStream, mut b: WebSocket<TcpStream>) {
+    loop {
+        let mut buffer = String::new();
+        a.read_to_string(&mut buffer).unwrap();
+        b.write_message(Message::text(buffer)).unwrap();
+    }
+}
+
+fn run_daemon() {
+    let server = TcpListener::bind("127.0.0.1:3030").unwrap();
+
+    let mut open_sockets: HashMap<&str, WebSocket<TcpStream>> = HashMap::new();
+    let mut cli_sockets: HashMap<&str, TcpStream> = HashMap::new();
+
+    for stream in server.incoming() {
+        let stream = stream.unwrap();
+        match accept(stream.try_clone().unwrap()) {
+            Ok(mut ws) => {
+                let name = "tmpname1232";
+                ws.write_message(Message::text("welcome")).unwrap();
+                if let Some(socket) = cli_sockets.remove(name) {
+                    std::thread::spawn(move || forward(socket, ws));
+                } else {
+                    open_sockets.insert(name, ws);
+                }
+            }
+            Err(_) => {
+                let name = "tmpname1232";
+                if let Some(ws) = open_sockets.remove(name) {
+                    std::thread::spawn(move || forward(stream, ws));
+                } else {
+                    cli_sockets.insert(name, stream);
+                }
+            }
+        }
+    }
+}
+
+fn client(mut stream: TcpStream) {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        stream.write(line.unwrap().as_bytes()).unwrap();
+    }
+}
 
 fn main() {
     App::new("DashPipe")
@@ -12,15 +63,31 @@ fn main() {
         .about("Pipes command line data to dashberry.ml")
         .get_matches();
 
-    let server = TcpListener::bind("127.0.0.1:3030").unwrap();
-    for stream in server.incoming() {
-        let mut websocket = accept(stream.unwrap()).unwrap();
-        websocket.write_message(Message::text("welcome")).unwrap();
-        let stdin = io::stdin();
-        for line in stdin.lock().lines() {
-            let line = line.expect("Could not read line from standard in");
-            websocket.write_message(Message::text(line + "\n")).unwrap();
+    let stream = TcpStream::connect("127.0.0.1:3030");
+
+    match stream {
+        Ok(stream) => {
+            println!("Daemon already running");
+            client(stream);
         }
-        break;
+        Err(_) => match fork().unwrap() {
+            Fork::Parent(_) => {
+                println!("Starting daemon and sleeping 500ms");
+                sleep(Duration::from_millis(500));
+                let stream = TcpStream::connect("127.0.0.1:3030").unwrap();
+                client(stream);
+            }
+            Fork::Child => {
+                println!("[daemon] starting");
+                if let Ok(_) = setsid() {
+                    chdir().unwrap();
+                    close_fd().unwrap();
+                    if let Ok(Fork::Child) = fork() {
+                        run_daemon();
+                    }
+                    // Do we care about the parent here??
+                }
+            }
+        },
     }
 }
