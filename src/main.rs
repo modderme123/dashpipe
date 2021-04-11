63a2bf5adfa17cd80c70400139fcf4335bb4dc5a
modderme123 <modderme123@gmail.com>
2021-04-10 22:13:08 -0700

    tokio the world

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,50 @@
-extern crate clap;
 use clap::App;
-use std::net::{TcpListener, TcpStream};
-use std::{
-    collections::HashMap,
-    io::{self, BufRead},
+use futures_util::{io::AsyncWriteExt as AsyncWriteExt2, SinkExt, StreamExt};
+use std::{collections::HashMap, error::Error, thread::sleep, time::Duration};
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
-use std::{io::prelude::*, thread::sleep, time::Duration};
-use tungstenite::Message;
-use tungstenite::{server::accept, WebSocket};
+use tokio_tungstenite::{accept_async, tungstenite::Message, WebSocketStream};
+use tokio_util::{compat::TokioAsyncWriteCompatExt, io::ReaderStream};
 
 use fork::{chdir, close_fd, fork, setsid, Fork};
 
-fn forward(mut a: TcpStream, mut b: WebSocket<TcpStream>) {
+async fn forward(mut a: TcpStream, mut b: WebSocketStream<TcpStream>) {
     let mut buffer = [0u8; 256];
     loop {
-        let bytes = a.read(&mut buffer[..]).unwrap();
+        let bytes = a.read(&mut buffer[..]).await.unwrap();
         if bytes > 0 {
-            b.write_message(Message::binary(buffer[0..bytes].to_vec()))
+            b.send(Message::binary(buffer[0..bytes].to_vec()))
+                .await
                 .unwrap();
         }
     }
 }
 
-fn run_daemon() {
-    let server = TcpListener::bind("127.0.0.1:3030").unwrap();
+#[tokio::main]
+async fn run_daemon() {
+    let server = TcpListener::bind("127.0.0.1:3030").await.unwrap();
 
-    let mut web_sockets: HashMap<&str, WebSocket<TcpStream>> = HashMap::new();
+    let mut web_sockets: HashMap<&str, WebSocketStream<TcpStream>> = HashMap::new();
     let mut cli_sockets: HashMap<&str, TcpStream> = HashMap::new();
 
-    for stream in server.incoming() {
-        let stream = stream.unwrap();
+    while let Ok((stream, _)) = server.accept().await {
         let mut front = [0u8; 14];
-        stream.peek(&mut front).expect("peek failed");
+        stream.peek(&mut front).await.expect("peek failed");
         if front == b"GET / HTTP/1.1".to_owned() {
-            let ws = accept(stream.try_clone().unwrap()).unwrap();
+            let ws = accept_async(stream).await.unwrap();
 
             let name = "tmpname1232";
             if let Some(socket) = cli_sockets.remove(name) {
-                std::thread::spawn(move || forward(socket, ws));
+                tokio::spawn(forward(socket, ws));
             } else {
                 web_sockets.insert(name, ws);
             }
         } else {
             let name = "tmpname1232";
             if let Some(ws) = web_sockets.remove(name) {
-                std::thread::spawn(move || forward(stream, ws));
+                tokio::spawn(forward(stream, ws));
             } else {
                 cli_sockets.insert(name, stream);
             }
@@ -52,12 +52,17 @@ fn run_daemon() {
     }
 }
 
-fn client(mut stream: TcpStream) {
-    stream.write(b"welcome, testing 123").unwrap();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        stream.write(line.unwrap().as_bytes()).unwrap();
-    }
+#[tokio::main]
+async fn client() -> Result<(), Box<dyn Error>> {
+    let mut stream = TcpStream::connect("127.0.0.1:3030").await?;
+    println!("[client] Connected to daemon");
+    stream.write(b"welcome, testing 123").await.unwrap();
+    let stdin = ReaderStream::new(io::stdin());
+    stdin
+        .forward(stream.compat_write().into_sink())
+        .await
+        .unwrap();
+    Ok(())
 }
 
 fn main() {
@@ -67,19 +72,12 @@ fn main() {
         .about("Pipes command line data to dashberry.ml")
         .get_matches();
 
-    let stream = TcpStream::connect("127.0.0.1:3030");
-
-    match stream {
-        Ok(stream) => {
-            println!("Daemon already running");
-            client(stream);
-        }
-        Err(_) => match fork().unwrap() {
+    if let Err(_) = client() {
+        match fork().unwrap() {
             Fork::Parent(_) => {
                 println!("Starting daemon and sleeping 500ms");
                 sleep(Duration::from_millis(500));
-                let stream = TcpStream::connect("127.0.0.1:3030").unwrap();
-                client(stream);
+                client().unwrap();
             }
             Fork::Child => {
                 println!("[daemon] starting");
@@ -91,6 +89,6 @@ fn main() {
                     }
                 }
             }
-        },
+        }
     }
 }
