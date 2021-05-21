69340a4a3d6a809912b3ded4257bc76726b2a6cc
lee <lee@underneath.ca>
2021-05-20 20:37:37 -0700

    simplify with select

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,112 @@
 use clap::{App, Arg};
-use fork::{chdir, close_fd, fork, setsid, Fork};
+use fork::{chdir, fork, setsid, Fork};
 use futures_util::{io::AsyncWriteExt as AsyncWriteExt2, StreamExt};
+use futures_util::{SinkExt};
 use log::*;
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
-use std::{collections::HashMap, error::Error, thread::sleep, time::Duration};
-use tokio::{
-    io::{self, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-};
+use std::{collections::HashMap, net::SocketAddr, option::Option};
+use std::{error::Error, thread::sleep, time::Duration};
+use tokio::{io::{self, AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}, task::JoinHandle};
+
+use futures_util::stream::FuturesUnordered;
 use tokio_tungstenite::{accept_async, tungstenite::Message, WebSocketStream};
 use tokio_util::{compat::TokioAsyncWriteCompatExt, io::ReaderStream};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 fn server_address(port: u16) -> String {
     format!("localhost:{}", port)
 }
 
-async fn forward(a: TcpStream, b: WebSocketStream<TcpStream>) {
-    let message_stream = ReaderStream::new(a).map(|x| Ok(Message::binary(x.unwrap().to_vec())));
-    message_stream.forward(b).await.unwrap();
+async fn forward(mut a: TcpStream, mut b: WebSocketStream<TcpStream>) {
+    let version = a.read_u16().await.unwrap();
+    assert_eq!(version, 1);
+    let header_size = a.read_u16().await.unwrap();
+    let mut buf = vec![0u8; header_size as usize];
+    let header_json = a.read_exact(&mut buf).await.unwrap();
+    info!("[server] received header size {}", header_size);
+    info!("[server] received header {:?}", header_json);
+    let mut header_buffer = vec![0u8; header_size as usize + 4];
+    header_buffer[0..2].copy_from_slice(&version.to_be_bytes());
+    header_buffer[2..4].copy_from_slice(&header_size.to_be_bytes());
+    header_buffer[4..].copy_from_slice(&buf);
+
+    info!("[server] header buffer: {:?}", &header_buffer);
+
+    let header_message = Message::binary(header_buffer);
+    b.send(header_message).await.unwrap();
+
+    let reader_stream = ReaderStream::new(a);
+    let logged_stream = reader_stream.map(|x| {
+        info!("[server] {:?}", x);
+        x
+    });
+    let message_stream = logged_stream.map(|x| Ok(Message::binary(x.unwrap().to_vec())));
+    let logged_messages = message_stream.map(|m| {
+        info!("[server] message");
+        m
+    });
+    logged_messages.forward(b).await.unwrap();
+    info!("[server] done");
 }
 
 #[tokio::main]
 async fn run_daemon(port: u16) {
     let server = TcpListener::bind(server_address(port)).await.unwrap();
 
-    let mut web_sockets: HashMap<&str, WebSocketStream<TcpStream>> = HashMap::new();
-    let mut cli_sockets: HashMap<&str, TcpStream> = HashMap::new();
+    let web_sockets: HashMap<&str, WebSocketStream<TcpStream>> = HashMap::new();
+    let cli_sockets: HashMap<&str, TcpStream> = HashMap::new();
+    let mut waits= FuturesUnordered::new();
+    let web_ref = Arc::new(Mutex::new(web_sockets));
+    let cli_ref = Arc::new(Mutex::new(cli_sockets));
 
-    while let Ok((stream, _)) = server.accept().await {
-        let mut front = [0u8; 14];
-        stream.peek(&mut front).await.expect("peek failed");
-        if front == *b"GET / HTTP/1.1" {
-            let ws = accept_async(stream).await.unwrap();
-
-            let name = "tmpname1232";
-            if let Some(socket) = cli_sockets.remove(name) {
-                tokio::spawn(forward(socket, ws));
-            } else {
-                web_sockets.insert(name, ws);
+    loop {
+        tokio::select! {
+            Ok(cxn) = server.accept() => {
+              let fwd = handle_connect(cxn, web_ref.clone(), cli_ref.clone()).await;
+              match fwd {
+                  Some(join_handle) => waits.push(join_handle),
+                  _ => ()
+              }
+            },
+            Some(x) = waits.next() => {
+                println!("[daemon loop] fwd complete, {:?}", x);
             }
+        }
+    }
+}
+
+async fn handle_connect(
+    cxn: (TcpStream, SocketAddr),
+    web_sockets_ref: Arc<Mutex<HashMap<&str, WebSocketStream<TcpStream>>>>,
+    cli_sockets_ref: Arc<Mutex<HashMap<&str, TcpStream>>>,
+) -> Option<JoinHandle<()>> {
+    let (stream, _) = cxn;
+    let mut cli_sockets = cli_sockets_ref.lock().await;
+    let mut web_sockets = web_sockets_ref.lock().await;
+
+    let mut front = [0u8; 14];
+    stream.peek(&mut front).await.expect("peek failed");
+    if front == *b"GET / HTTP/1.1" {
+        let ws = accept_async(stream).await.unwrap();
+
+        let name = "tmpname1232";
+        if let Some(socket) = cli_sockets.remove(name) {
+            let handle = tokio::spawn(forward(socket, ws));
+            return Some(handle)
         } else {
-            let name = "tmpname1232";
-            if let Some(ws) = web_sockets.remove(name) {
-                tokio::spawn(forward(stream, ws));
-            } else {
-                cli_sockets.insert(name, stream);
-            }
+            web_sockets.insert(name, ws);
+            return None;
+        }
+    } else {
+        let name = "tmpname1232";
+        if let Some(ws) = web_sockets.remove(name) {
+            let handle = tokio::spawn(forward(stream, ws));
+            return Some(handle);
+        } else {
+            cli_sockets.insert(name, stream);
+            return None;
         }
     }
 }
@@ -77,7 +137,7 @@ async fn client(port: u16, args: &PipeArgs) -> Result<(), Box<dyn Error>> {
     let length = header_vec.len() as u16;
     let length_bytes = length.to_be_bytes();
     let header_str = serde_json::to_string(&args)?;
-    info!("header: {}", &header_str);
+    info!("[client] header: {}", &header_str);
 
     header.extend_from_slice(&PROTOCOL_VERSION);
     header.extend_from_slice(&length_bytes);
@@ -113,7 +173,7 @@ fn main() {
         .get_matches();
 
     // env_logger::init();
-    env_logger::builder().filter_level(LevelFilter::Info).init(); // for now turn all all logging
+    env_logger::builder().filter_level(LevelFilter::Info).init(); // for now set logging level in code
 
     let port_str = arg_matches.value_of("port").unwrap();
     let port: u16 = port_str.parse().unwrap();
@@ -128,7 +188,7 @@ fn main() {
     if client(port, &pipe_args).is_err() {
         match fork().unwrap() {
             Fork::Parent(_) => {
-                debug!("Starting daemon and sleeping 500ms");
+                debug!("Starting daemon and sleeping 500ms"); // TODO fix race condition
                 sleep(Duration::from_millis(500));
                 client(port, &pipe_args).unwrap();
             }
@@ -136,8 +196,9 @@ fn main() {
                 info!("[daemon] starting");
                 if setsid().is_ok() {
                     chdir().unwrap();
-                    close_fd().unwrap(); // comment out to enable debug logging in daemon
+                    // close_fd().unwrap(); // comment out to enable debug logging in daemon
                     if let Ok(Fork::Child) = fork() {
+                        println!("[forked] foo");
                         run_daemon(port);
                     }
                 }
